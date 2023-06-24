a3c072ac3ae75797578aa26f58d4cca00512e46e
lucaspari <lucaspariii@gmail.com>
2023-06-24 13:12:21 -0300

    build : building project

--- build/index.js
+++ build/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import connectDatabase from './config/database.js';
 const app = express();
 const port = 3000;
+connectDatabase();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json('Hello World');
 });
 app.listen(port, () => {
     console.log("Server is running on port 3000");
