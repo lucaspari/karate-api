13cbda45479c0b914b4afb8fe77043dac8ee5766
lucaspari <lucaspariii@gmail.com>
2023-07-02 16:48:56 -0300

    format: prettier

--- src/routes/golpeRoutes.ts
+++ src/routes/golpeRoutes.ts
@@ -12,17 +12,20 @@ golpeRouter.get('/', async (req: Request, res: Response) => {
   }
 });
 
-golpeRouter.get('/findByGolpeUrlPath/:golpe', async (req: Request, res: Response) => {
-  try {
-    const golpes = await Golpe.findOne({
-      urlPath: req.params.golpe,
-    });
-    res.json(golpes);
-  } catch (error) {
-    console.error('Error fetching golpes:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+golpeRouter.get(
+  '/findByGolpeUrlPath/:golpe',
+  async (req: Request, res: Response) => {
+    try {
+      const golpes = await Golpe.findOne({
+        urlPath: req.params.golpe,
+      });
+      res.json(golpes);
+    } catch (error) {
+      console.error('Error fetching golpes:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+);
 golpeRouter.get(
   '/findGolpesByFaixa/:faixa',
   async (req: Request, res: Response) => {
@@ -38,15 +41,15 @@ golpeRouter.get(
   },
 );
 
-golpeRouter.get('/randomGolpe/',async(req: Request, res: Response) => {
-  console.log('entrou')
-try{
-  const golpes = await Golpe.find();
-  const randomGolpe = golpes[Math.floor(Math.random() * golpes.length)];
-  return res.json(randomGolpe);
-}
-catch(error){
-  console.error('Error fetching golpes:', error);
-  res.status(500).json({ error: 'Internal Server Error' });
-}})
+golpeRouter.get('/randomGolpe/', async (req: Request, res: Response) => {
+  console.log('entrou');
+  try {
+    const golpes = await Golpe.find();
+    const randomGolpe = golpes[Math.floor(Math.random() * golpes.length)];
+    return res.json(randomGolpe);
+  } catch (error) {
+    console.error('Error fetching golpes:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 export default golpeRouter;
